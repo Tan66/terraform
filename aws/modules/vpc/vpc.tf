00d1603aef265fa87568dc66c358aed974ca4fe0
Tanveer Ahmed <tanveerahmed2619@gmail.com>
2022-12-06 20:03:19 +0530

    ecs asg with alb target group

--- aws/modules/vpc/vpc.tf
+++ aws/modules/vpc/vpc.tf
@@ -69,8 +69,35 @@ resource "aws_security_group" "allow" {
 
   ingress {
     description = "all"
-    from_port   = 0
-    to_port     = 0
+    from_port   = 32768
+    to_port     = 65535
+    protocol    = "tcp"
+    # TODO: change to 0.0.0.0/0
+    cidr_blocks = ["0.0.0.0/0"]
+    # ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+  }
+  ingress {
+    description = "ssh"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    # TODO: change to 0.0.0.0/0
+    cidr_blocks = ["0.0.0.0/0"]
+    # ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+  }
+  ingress {
+    description = "https"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    # TODO: change to 0.0.0.0/0
+    cidr_blocks = ["0.0.0.0/0"]
+    # ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+  }
+  ingress {
+    description = "https"
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     # TODO: change to 0.0.0.0/0
     cidr_blocks = ["0.0.0.0/0"]
--- aws/vpc-ec2-serverless/main.tf
+++ aws/vpc-ec2-serverless/main.tf
@@ -33,16 +33,81 @@ module "vpc" {
 
 #####################################################################################
 
-# data "aws_iam_policy_document" "this" {
-#     statement {
-#         actions = ["sts:AssumeRole"]
+## alb
 
-#         principals {
-#           type = "Service"
-#           identifiers = ["ec2.amazonaws.com"]
-#         }
-#     }
-# }
+resource "aws_lb" "this" {
+  name               = "test-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [module.vpc.allow_all_security_group_id]
+  subnets            = [for subnet in module.vpc.public_subnet_ids : subnet]
+  enable_http2 = false
+  enable_deletion_protection = false
+
+  access_logs {
+    bucket  = "alb-logs"
+    prefix  = "test-lb"
+    enabled = false
+  }
+
+  tags = {
+    "env" = "dev"
+  }
+}
+
+output "alb_dns" {
+  value = aws_lb.this.dns_name
+}
+
+## alb target group
+
+resource "aws_lb_target_group" "this" {
+  name     = "test-ecs-tg"
+  port     = 80
+  target_type = "instance"
+  protocol = "HTTP"
+  vpc_id   = module.vpc.vpc_id
+  health_check {
+    enabled = true
+    healthy_threshold = 3
+    interval = 30
+    path = "/"
+    port = "traffic-port"
+    protocol = "HTTP"
+    timeout = 5
+    unhealthy_threshold = 3
+  }
+  tags = {
+    "env" = "dev"
+  }
+}
+
+resource "aws_lb_listener" "thi" {
+  load_balancer_arn = aws_lb.this.arn
+  port              = "80"
+  protocol          = "HTTP"
+  # ssl_policy        = "ELBSecurityPolicy-2016-08"
+  # certificate_arn   = "arn:aws:iam::187416307283:server-certificate/test_cert_rab3wuqwgja25ct3n4jdj2tzu4"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.this.arn
+  }
+  tags = {
+    "env" = "dev"
+  }
+}
+
+data "aws_iam_policy_document" "this" {
+    statement {
+        actions = ["sts:AssumeRole"]
+
+        principals {
+          type = "Service"
+          identifiers = ["ec2.amazonaws.com"]
+        }
+    }
+}
 
 resource "aws_iam_role" "this" {
   name = "ecs-ec2-agent-role"
@@ -121,6 +186,12 @@ resource "aws_autoscaling_group" "ecs_ec2_asg" {
     id = aws_launch_template.this.id
     version = "$Latest"
   }
+  
+  tag {
+    key = "AmazonECSManaged"
+    value = ""
+    propagate_at_launch = true
+  }
 }
 
 # asg
@@ -153,6 +224,81 @@ module "ecs_ec2" {
     }
   }
 }
+
+## task definition
+## refer https://docs.aws.amazon.com/AmazonECS/latest/APIReference/API_ContainerDefinition.html
+resource "aws_ecs_task_definition" "this" {
+  family = "test"
+  container_definitions = jsonencode(
+    [
+      {
+        name = "nginx"
+        image = "nginx"
+        cpu = 0
+        memory = 300
+        memoryReservation = 256
+        essential = true
+        environment = []
+        mountPoints = []
+        privilaged = false
+        secrets = []
+        volumesFrom = []
+        portMappings = [
+          {
+            containerPort = 80
+            hostPort = 0
+            protocol = "tcp"
+          }
+        ]
+
+      }
+    ]
+  )
+
+  # EC2 or FARGATE
+  requires_compatibilities = [
+    "EC2"
+  ]
+  
+  tags = {
+    "env" = "dev"
+  }
+}
+
+## ecs service
+
+data "aws_iam_role" "ecs_service_role" {
+  name = "AWSServiceRoleForECS"
+}
+
+resource "aws_ecs_service" "this" {
+  name = "nginx"
+  cluster = module.ecs_ec2.cluster_name
+  task_definition = aws_ecs_task_definition.this.arn
+  desired_count = 1
+  enable_execute_command = false
+  force_new_deployment = true
+  launch_type = "EC2" # EC2, FARGATE, EXTERNAL
+  scheduling_strategy = "REPLICA" # REPLICA or DAEMON
+  load_balancer {
+    target_group_arn = aws_lb_target_group.this.arn
+    container_name = "nginx"
+    container_port = 80
+  }
+  iam_role = data.aws_iam_role.ecs_service_role.arn
+  tags = {
+    "env" = "dev"
+  }
+  
+}
+
+
+
+
+
+
+
+
 
 #########################
 # not all feature available in launch configuration
